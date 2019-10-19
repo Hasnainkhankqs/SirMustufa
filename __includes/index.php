e95785cbf3d1bdd715ff8d802b22a04da6dc9fec
hasnainkhan <hasnainkhankqs@gmail.com>
2019-10-19 23:00:34 +0500

    some changes2

--- __includes/index.php
+++ __includes/index.php
@@ -1,52 +1,6 @@
 <!doctype html>
 <html>
 <style>
-#innerbox,#innerbox2{
-	display: none;
-	position: absolute;
-    width: 100%;
-	height: 100%;
-	top:0;
-	left:0;
-    padding:1.5rem;
-	-webkit-animation-name: animatetop;
-  -webkit-animation-duration: 0.4s;
-  animation-name: animatetop;
-  animation-duration: 0.4s
-}
-#SearchBox p,#SearchBox2 p{
-	user-select: none;
-}
-
-#innerbox2 .btn-success{
-	background: #5d50c6;
-	border-color: #3e3394;
-}
-#innerbox2 .btn-success:hover{
-	border-color: #292073;
-
-}
-#SearchBox,#SearchBox2{
-	cursor : pointer;
-}
-
-#searchSchbtn{
-	background:#8cc152;
-}
-
-
-
-
-/* Add Animation */
-@-webkit-keyframes animatetop {
-  from {top:-300px; opacity:0;background-color: rgba(0,0,0,0.5);} 
-  to {top:0; opacity:1;background-color: rgba(0,0,0,0)}
-}
-
-@keyframes animatetop {
-	from {top:-300px; opacity:0;background-color: rgba(0,0,0,0.5);} 
-  to {top:0; opacity:1;background-color: rgba(0,0,0,0)}
-}
 
 
 
@@ -72,13 +26,10 @@ include('header.php');
 // Get the modal
 var modal = document.getElementById("innerbox");
 var modal2 = document.getElementById("innerbox2");
-// modal.style.display = "flex";
 // Get the button that opens the modal
 var btn = document.getElementById("SearchBox");
 var btn2 = document.getElementById("SearchBox2");
 
-// Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
@@ -88,11 +39,7 @@ btn2.onclick = function() {
   modal2.style.display = "flex";
 }
 
-// When the user clicks on <span> (x), close the modal
-	// span.onclick = function() {
-	// 	modal.style.display = "none";
-	// }
-	
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
